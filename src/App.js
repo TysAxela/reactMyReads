0d0239c64cddc8666f09567649173b5fa75fc60f
Ty <tysaxela@gmail.com>
2017-12-24 05:34:22 +0000

    All of react router is working (route and link)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react'
 // import * as BooksAPI from './BooksAPI'
 import './App.css'
-import { Route } from 'react-router-dom'
+import { Route, Link } from 'react-router-dom'
+import Header from './Header'
 import CurrentlyRding from './CurrentlyRding'
 import WantToRead from './WantToRead'
 import Read from './Read'
@@ -15,33 +16,33 @@ class BooksApp extends React.Component {
      * we're on, use the URL in the browser's address bar. This will ensure that
      * users can use the browser's back and forward buttons to navigate between
      * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
+     */    
   }
 
-  render() {
-    return (
-      <div className="app">
-       <Route exact path='/' render={() => (
-		<div className="list-books">
-			<div className="list-books-title">
-              <h1>MyReads</h1>
-            </div>
-            <div className="list-books-content">
-            	<div>          
-                	<CurrentlyRding />
-                	<WantToRead />
-                	<Read />
-				</div>          
-            <div className="open-search">
-              <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
-            </div>
-          </div>
-		</div>
-		)}/>
-      </div>
-    )
-  }
+	render() {
+		return (
+			<div className='app'>
+				<Route exact path='/' render={() => (
+					<div className='list-books'>
+						<Header />
+						<div className='list-books-content'>
+            				<div>
+								<CurrentlyRding />
+								<WantToRead />
+								<Read />
+							</div>
+							<div className='open-search'>
+								<Link to='/search'>Add a book</Link>
+							</div>
+						</div>
+					</div>
+				)}/>
+				<Route path='/search' render={({ history }) => (
+					<Search />
+                )}/>
+			</div>
+		)
+	}
 }
 
 export default BooksApp
--- src/Search.js
+++ src/Search.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 import './App.css'
 
 class Search extends Component {
@@ -6,7 +7,7 @@ class Search extends Component {
     return (
 		<div className="search-books">
 			<div className="search-books-bar">
-				<a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
+				<Link className="close-search" to='/'>Close</Link>
 				<div className="search-books-input-wrapper">
                   {/*
                     NOTES: The search from BooksAPI is limited to a particular set of search terms.
