1d96e92cb7a2f651421bb4dfa69d04ffaa4d08ce
Ty <tysaxela@gmail.com>
2017-12-24 01:58:32 +0000

    added three files each with one compenent for the bookshelf sections

---
+++ src/CurrentlyRding.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+class CurrentlyRding extends Component {
+	render() {
+      return (
+      )
+    }
+}
+
+export default CurrentlyRding
---
+++ src/WantToRead.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+class WantToRead extends Component {
+	render() {
+      return (
+      )
+    }
+}
+
+export default WantToRead
